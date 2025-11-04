3bbb1215c255233320124d930ea60a7d9c8133e9
Rebeccagrites <rebecca.grites@escola.pr.gov.br>
2025-11-04 10:40:49 -0300

    dd

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+// Rolagem suave
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
 
